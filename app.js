eeaf6f1479c88bfda96523eccbc12e79bf327e07
JosephJasin <josef20606@gmail.com>
2021-11-26 13:49:06 +0200

    change the error returned by express.json() from html to json

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const httpServer = http.createServer(app);
 app.use(express.json());
 
 app.use((err, req, res , next) => {
-    res.status(400).send("Invalid Json format");
+    res.status(400).json("Invalid Json format");
 }
 );
 
